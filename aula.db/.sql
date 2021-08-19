ebada8c91f23858739a2b6048b5b625117969206
Hrolfr <sabrina.sales163@gmail.com>
2021-08-19 13:11:42 -0300

    mais conteudo

--- aula.db/.sql
+++ aula.db/.sql
@@ -201,6 +201,96 @@ GROUP BY tipo_transacao_id
 ORDER BY tipo_transacao_id DESC;
 --descendente
 
+-------------------------------------------------------------------
+
+SELECT numero,nome FROM banco;
+SELECT banco_numero,numero, nome FROM agencia;
+SELECT numero,nome FROM cliente;
+SELECT banco_numero,agencia_numero,numero,digito,cliente_numero FROM conta_corrente;
+SELECT id,nome FROM tipo_transação;
+SELECT banco_numero,agencia_numero,conta_corrente_numero,conta_corrente_digito,cliente_numero,valor FROM cliente_transacoes;
+
+SELECT COUNT(1) FROM banco; --151 bancos
+SELECT COUNT(1) FROM agencia; --296 agencias
+SELECT 1 FROM agencia; --trás 1 em todos os registros
+--SELECT COUNT(id) ajuda,mas (1) é melhor
+
+--ele coloca COUNT(1) e não asterisco pois ele só quer contar o número de registros que ele retornará
+
+--296 registros
+SELECT banco.numero,banco.nome,agencia.numero,agencia.nome
+FROM banco
+JOIN agencia ON agencia.banco_numero = banco.numero;
+
+SELECT banco.numero
+FROM banco
+JOIN agencia ON agencia.banco_numero = banco.numero
+GROUP BY banco.numero;
+
+--9
+SELECT count (distinct banco.numero)
+FROM banco
+JOIN agencia ON agencia.banco_numero = banco.numero;
+
+--438
+SELECT banco.numero, banco.nome,agencia.numero,agencia.nome
+FROM banco
+LEFT JOIN agencia ON agencia.banco_numero = banco.numero;
+--retornou todos os bancos que possuem relacionamento com as agencias e todos sem relaçao nenhuma
+--depois do 296 é só nulo
+
+--438
+SELECT agencia.numero,agencia.nome,banco.numero, banco.nome
+FROM agencia
+RIGHT JOIN banco ON  banco.numero = agencia.banco_numero ;
+--depois do 296,null
+--só funcionou pq invertemos as tabelas
+
+--293?
+SELECT agencia.numero,agencia.nome,banco.numero, banco.nome
+FROM agencia
+LEFT JOIN banco ON  banco.numero = agencia.banco_numero;
+
+--434?
+SELECT banco.numero,banco.nome,agencia.numero,agencia.nome
+FROM banco
+FULL JOIN agencia ON  agencia.banco_numero = banco.numero;
+--Funciona como o LEFT JOIN
+
+CREATE TABLE IF NOT EXISTS teste_a(id serial primary key, valor varchar(10));
+CREATE TABLE IF NOT EXISTS teste_b(id serial primary key, valor varchar(10));
+ 
+INSERT INTO teste_a (valor) VALUES ('teste1');
+INSERT INTO teste_a (valor) VALUES ('teste2');
+INSERT INTO teste_a (valor) VALUES ('teste3');
+INSERT INTO teste_a (valor) VALUES ('teste4');
+
+INSERT INTO teste_b (valor) VALUES ('teste_a');
+INSERT INTO teste_b (valor) VALUES ('teste_b');
+INSERT INTO teste_b (valor) VALUES ('teste_c');
+INSERT INTO teste_b (valor) VALUES ('teste_d');
+
+SELECT tbla.valor, tblb.valor
+FROM teste_a tbla
+CROSS JOIN teste_b tblb;
+
+DROP TABLE IF EXISTS teste_a;
+DROP TABLE IF EXISTS teste_b;
+
+SELECT banco.nome,
+       agencia.nome,
+	   conta_corrente.numero,
+       conta_corrente.digito,
+	   cliente.nome
+FROM banco 
+JOIN agencia ON agencia.banco_numero = banco.numero
+JOIN conta_corrente
+   --ON conta_corrente.banco_numero = agencia.banco_numero
+    ON conta_corrente.banco_numero = banco.numero
+	AND conta_corrente.agencia_numero = agencia.numero
+JOIN cliente
+   ON cliente.numero = conta_corrente.cliente_numero;
+--join entre 4 tabelas
 
 
 
